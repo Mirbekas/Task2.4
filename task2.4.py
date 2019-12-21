123f52785c5a3879be683873fddc9408e400bc27
Mirbek <md.mirbek@gmail.com>
2019-12-21 12:05:29 +0600

    task

---
+++ task2.4.py
@@ -0,0 +1,9 @@
+a = int(input('hour = '))
+b = int(input('minute = '))
+c = int(input('seconds = '))
+a1 = int(input('hour1 = '))
+b1 = int(input('minute1 = '))
+c1 = int(input('seconds1 = '))
+c2 = c1-c
+c3 = c2+60
+print(c3,'sec')
