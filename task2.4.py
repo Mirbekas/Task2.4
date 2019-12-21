54df4989a796ea83b98dea3cf38e3885c86aea6e
Mirbek <md.mirbek@gmail.com>
2019-12-21 12:47:55 +0600

    task

--- task2.4.py
+++ task2.4.py
@@ -4,6 +4,7 @@ c = int(input('seconds = '))
 a1 = int(input('hour1 = '))
 b1 = int(input('minute1 = '))
 c1 = int(input('seconds1 = '))
-c2 = c1-c
-c3 = c2+60
-print(c3,'sec')
+abc = (a*60*60)+(b*60)+c
+abc1 = (a1*60*60)+(b1*60)+c1
+time = abc - abc1
+print(abs(time),'sec')
